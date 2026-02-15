c8aba4066b30b4eb16901a45f04aaa1c89f21810
apoorva-edustreamix <apoorva300705@gmail.com>
2026-02-15 18:16:21 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,132 @@
+const commonSubjects = [
+
+{
+name:"English",
+channels:[
+{name:"Part A",links:[
+"https://youtube.com/playlist?list=plujxb7al70ib4mdwqipyrirrylnwjpu2d&si=8phgrnbp-fnohpk7"
+]},
+{name:"Part B",links:[
+"https://youtube.com/playlist?list=plujxb7al70ibdke4oii4d40mycuq4jnq8&si=oh-fwjsijgc51ib7"
+]},
+{name:"Whole Subject",links:[
+"https://youtube.com/playlist?list=pljndhmbtsxpwayvg7o59hqscuykkhvz-y&si=oyqtupka4_ikqzyi",
+"https://youtube.com/playlist?list=plyjvm5gfnmy6rkpqkph7psuaobb44shws&si=fipxj6zwuoakyb6n",
+"https://youtube.com/playlist?list=plldwy7gu4r757w2-mkgfz7hdzdvhp-gkt&si=eebbghwtav4mhoas"
+]}
+]
+},
+
+{
+name:"Telugu",
+channels:[
+{name:"Whole Subject",links:[
+"https://youtube.com/playlist?list=plyjvm5gfnmy6uua5iln-gqnpkw7y1cemh&si=rdh0wzkud8f6dnua",
+"https://youtube.com/playlist?list=pl7nokdjp4zrkvkwtolt6edyqabvc-zjni&si=ieimcfyaqrcdeclh"
+]},
+{name:"Short Revision",links:[
+"https://youtube.com/playlist?list=plbjrd3cfcesrmenf5fltuoxug3kjjdppx&si=-eswtlfzmzvlt6sj"
+]}
+]
+},
+
+{
+name:"Hindi",
+channels:[
+{name:"Whole Subject",links:[
+"https://youtube.com/playlist?list=plstbuofp6ehdnt2reau2eaprgmdw6bzra&si=yz2i_uoylfasx8wy"
+]}
+]
+},
+
+{
+name:"Sanskrit",
+channels:[
+{name:"Whole Subject",links:[
+"https://youtube.com/playlist?list=plyjvm5gfnmy7ynznz0neh3lcgai1obzhh&si=loiayfkigrhnsmhu",
+"https://youtube.com/playlist?list=plc1q__0qogaoauv-llipynqpatmye_7kj&si=_7d_msv4c2jqih19"
+]}	
+]
+},
+
+{
+name:"Cost Control and Management Accounting",
+channels:[
+{name:"Whole Subject",links:[
+"https://youtube.com/playlist?list=Pllhsiffdzcuvabugimvqfx1ovgzfhbpec&si=Kngahxohhlsrvbgd"
+]},
+{name:"Part 1",links:[
+"https://youtube.com/playlist?list=Plwjdzvupkxallmgp19jfhnenudgoy1g2v&si=Rznoqj450u2a1utd"
+]},
+{name:"Part 2",links:[
+"https://youtube.com/playlist?list=Plwjdzvupkxalg4vne3zhb7uvp9jwl6mog&si=Q-W4d1m7q0i3_3oh"
+]}
+]
+},
+
+{
+name:"Theory and Practice of GST",
+channels:[
+{name:"Whole Subject",links:[
+"https://youtube.com/playlist?list=Plwjdzvupkxaknpdaiqufc2jxdoxjenrsj&si=Wvl4inxhtawnbqfi"
+]}
+]
+},
+
+{
+name:"Accounting Standards",
+channels:[
+{name:"Whole Subject",links:[
+"https://youtube.com/playlist?list=PLLhSIFfDZcUXO81uWFy_F6O4ZX0JfTYkV&si=sveBQraIQh_mg749",
+"https://youtube.com/playlist?list=PLWJDzVuPkXAmAI6CfaLfYpQVvQFhXzxC6&si=THNBbXF7EUDDW2as"
+]}
+]
+},
+
+{
+name:"Research Methodology & Project",
+channels:[
+{name:"Whole Subject",links:[
+"https://youtube.com/playlist?list=pllhsiffdzcuwrlgixmkd1rnelsz1you4o&si=u6bc6ow1d7eoi3g3",
+"https://youtube.com/playlist?list=PLk3poRqYftlhDGto5oSxeNeFWHzajbVuA&si=3MwDnvSrUd3EVlXN",
+"https://youtube.com/playlist?list=PLjMjkIR4-dvIAvboLjtq6sSi_1_wk2VgD&si=vLv1v5kmXAh1_pnU"
+]}
+]
+}
+
+];
+
+const data = {
+
+"BCom General - Semester 6": commonSubjects,
+
+"BCom Honors - Semester 6": [
+...commonSubjects,
+{
+name:"International Finance",
+channels:[
+{name:"Whole Subject",links:[
+"https://youtube.com/playlist?list=PLWJDzVuPkXAnNpRjN6InK65R7ndvpYZUf&si=EDV35BDJUJIhuLgc"
+]}
+]
+}
+],
+
+/* Accounting Standards Removed */
+"BCom Computer Applications - Semester 6":[
+...commonSubjects.filter(sub => sub.name !== "Accounting Standards"),
+{
+name:"Cybersecurity",
+channels:[
+{name:"Whole Subject",links:[
+"https://youtube.com/playlist?list=PLtfg0YD2YYUQMZp4bRkeZI0JXWHGqh8vo&si=uQhVzmPNV1kv5LtJ"
+]}
+]
+}
+],
+
+/* Accounting Standards Removed */
+"BCom Business Analytics - Semester 6":
+commonSubjects.filter(sub => sub.name !== "Accounting Standards")
+
+};
